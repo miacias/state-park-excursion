78ca7839706db6fa9194df7b5f3872a7b46ded87
mikestem <michael.a.stem@gmail.com>
2023-01-08 13:11:38 -0500

    added state park fetch

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const stateParkapiKey = "CBfyxbdetzhPX1Eb6AkF8tKog9tRDva0gzXJylB8"
+var stateParkFetchBtn = document.getElementById('fetch-stateParkInfo');
+
+function getStateParkAPI() {
+    var requestURL= 'https://developer.nps.gov/api/v1/parks?&api_key=CBfyxbdetzhPX1Eb6AkF8tKog9tRDva0gzXJylB8';
+
+    fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data){
+     console.log(data)
+    })
+}
+
+stateParkFetchBtn.addEventListener('click', getStateParkAPI);
+
+
+
