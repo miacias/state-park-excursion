886245ad20c66f3ca2ab1d8bfa17d06dce8e36f6
miacias <miaciasullo@gmail.com>
2023-01-09 22:42:40 -0500

    carousel needs slight adjustments. classes added and local CSS linked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,34 @@
 
 // NATIONAL PARK SERVICES API
-const stateParkapiKey = "CBfyxbdetzhPX1Eb6AkF8tKog9tRDva0gzXJylB8"
-var stateParkFetchBtn = document.getElementById('fetch-stateParkInfo');
+    const stateParkapiKey = "CBfyxbdetzhPX1Eb6AkF8tKog9tRDva0gzXJylB8"
+    var stateParkFetchBtn = document.getElementById('fetch-stateParkInfo');
 
-function getStateParkAPI() {
-    var requestURL= 'https://developer.nps.gov/api/v1/parks?&api_key=CBfyxbdetzhPX1Eb6AkF8tKog9tRDva0gzXJylB8';
+    function getStateParkAPI() {
+        var requestURL= 'https://developer.nps.gov/api/v1/parks?&api_key=CBfyxbdetzhPX1Eb6AkF8tKog9tRDva0gzXJylB8';
 
-    fetch(requestURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data){
-     console.log(data)
-    })
-}
+        fetch(requestURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data){
+        console.log(data)
+        })
+    }
 
-stateParkFetchBtn.addEventListener('click', getStateParkAPI);
+    stateParkFetchBtn.addEventListener('click', getStateParkAPI);
 
 // IMAGE CAROUSEL
-var carousel = document.querySelector('.carousel');
-var instance = M.Carousel.getInstance(elems);
+    // loads safely after DOM is loaded
+    document.addEventListener('DOMContentLoaded', function() {
+        var carousel = document.querySelector('.carousel');
+        var options = {
+            fullWidth: true,
+            duration: 4000,
+            numVisible: 1
+        };
+        var instances = M.Carousel.init(carousel, options);
 
-// loads safely after DOM is loaded
-document.addEventListener('DOMContentLoaded', function() {
-    // var elems = document.querySelector('.carousel');
-    var options = 20000;
-    var instances = M.Carousel.init(carousel, options);
-    setInterval(function() {
-        instances.next();
-    }, 5000)
-  });
+        setInterval(function() {
+            instances.next();
+        }, 8000)
+    });
