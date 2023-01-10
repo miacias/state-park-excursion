2091058c817fbafe8aba81db526e14bc005d4d8a
miacias <miaciasullo@gmail.com>
2023-01-09 23:53:16 -0500

    changed ID of park fetch button to fetch-park-info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,47 @@
 
 // NATIONAL PARK SERVICES API
-    const stateParkapiKey = "CBfyxbdetzhPX1Eb6AkF8tKog9tRDva0gzXJylB8"
-    var stateParkFetchBtn = document.getElementById('fetch-stateParkInfo');
+const stateParkapiKey = "CBfyxbdetzhPX1Eb6AkF8tKog9tRDva0gzXJylB8"
+var stateParkFetchBtn = document.getElementById('fetch-park-info');
 
-    function getStateParkAPI() {
-        var requestURL= 'https://developer.nps.gov/api/v1/parks?&api_key=CBfyxbdetzhPX1Eb6AkF8tKog9tRDva0gzXJylB8';
+function getStateParkAPI() {
+    var requestURL= 'https://developer.nps.gov/api/v1/parks?&api_key=CBfyxbdetzhPX1Eb6AkF8tKog9tRDva0gzXJylB8';
 
-        fetch(requestURL)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data){
-        console.log(data)
-        })
-    }
+    fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data){
+    console.log(data)
+    })
+}
 
-    stateParkFetchBtn.addEventListener('click', getStateParkAPI);
+stateParkFetchBtn.addEventListener('click', getStateParkAPI);
 
 // IMAGE CAROUSEL
-    // DOMContentLoaded: loads safely after DOM is loaded
-    document.addEventListener('DOMContentLoaded', function() {
-        var carousel = document.querySelector('.carousel');
-        // built-in Materialize options
-        var options = {
-            fullWidth: true,
-            duration: 4000,
-            numVisible: 1,
-        };
-        var instances = M.Carousel.init(carousel, options);
-        // change picture every X milliseconds
-        setInterval(function() {
-            instances.next();
-        }, 8000)
-    });
+// DOMContentLoaded: loads safely after DOM is loaded
+document.addEventListener('DOMContentLoaded', function() {
+    var carousel = document.querySelector('.carousel');
+    // built-in Materialize options
+    var options = {
+        fullWidth: true,
+        duration: 4000,
+        numVisible: 1,
+    };
+    var instances = M.Carousel.init(carousel, options);
+    // change picture every X milliseconds
+    setInterval(function() {
+        instances.next();
+    }, 8000)
+});
+
+// STATES LIST AUTOCOMPLETE
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.autocomplete');
+    var instances = M.Autocomplete.init(elems, options);
+});
 
 // PARKS LIST DROPDOWN
-    document.addEventListener('DOMContentLoaded', function() {
-        var elems = document.querySelectorAll('.dropdown-trigger');
-        var instances = M.Dropdown.init(elems, options);
-    });
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, options);
+});
