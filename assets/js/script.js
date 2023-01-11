18fb02904321bdc6552768c2bb94aa7ab76a0cb0
mikestem <michael.a.stem@gmail.com>
2023-01-11 18:51:44 -0500

    api changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 const stateParkapiKey = "CBfyxbdetzhPX1Eb6AkF8tKog9tRDva0gzXJylB8"
 var stateParkFetchBtn = document.getElementById('fetch-stateParkInfo');
-
+var parkList = document.querySelector('ul');
+var state = ""
 function getStateParkAPI() {
-    var requestURL= 'https://developer.nps.gov/api/v1/parks?&api_key=CBfyxbdetzhPX1Eb6AkF8tKog9tRDva0gzXJylB8';
+    var requestURL= 'https://developer.nps.gov/api/v1/visitorcenters?state=' + state + '&api_key=CBfyxbdetzhPX1Eb6AkF8tKog9tRDva0gzXJylB8';
 
     fetch(requestURL)
     .then(function (response) {
@@ -11,6 +12,8 @@ function getStateParkAPI() {
     .then(function (data){
      console.log(data)
     })
+
 }
+
 
 stateParkFetchBtn.addEventListener('click', getStateParkAPI);
