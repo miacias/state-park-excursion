eda368a17edd1332349c5a736fbd15cd874147c5
Mia Ciasullo <miaciasullo@gmail.com>
2023-01-17 19:52:05 -0500

    fixed class name of key card input field

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,7 @@
 var buttonEl = document.getElementById("save-key");
 var inputEl = document.getElementsByClassName("map-key-input");
 var keyCardEl = document.getElementById("key-card-container");
+var goButton = document.getElementById("fetch-park-info");
 
 function saveKeyToStorage() {
     localStorage.setItem('map-key', inputEl[0].value);
@@ -14,9 +15,11 @@ buttonEl.addEventListener("click", function(event) {
     if (localStorage.getItem("map-key")) {
         if (localStorage.getItem("map-key").length === 39) {
             keyCardEl.classList.add("hide");
+            goButton.classList.remove("tooltipped");
         }
     }
     if (!localStorage.getItem("map-key")) {
         keyCardEl.classList.remove("hide");
+        goButton.classList.add("tooltipped");
     }
 })
